b1a783f53640002b48440b1efe7a06fd10540067
Omer <639682+omercnet@users.noreply.github.com>
2026-08-06 23:55:46 +0000

    fix(cosign): restore vulnerability-clean package

--- images/cosign/tests/test.sh
+++ images/cosign/tests/test.sh
@@ -8,7 +8,7 @@ user=$(id -u):$(id -g)
 printf '%s\n' 'signed payload' >"$tmp/blob"
 
 docker run --rm --user "$user" -e HOME=/work -e COSIGN_PASSWORD= -v "$tmp:/work" -w /work "$image" generate-key-pair
-docker run --rm --user "$user" -e HOME=/work -e COSIGN_PASSWORD= -v "$tmp:/work" -w /work "$image" sign-blob --yes --tlog-upload=false --key cosign.key --output-signature blob.sig blob
+docker run --rm --user "$user" -e HOME=/work -e COSIGN_PASSWORD= -v "$tmp:/work" -w /work "$image" sign-blob --yes --use-signing-config=false --new-bundle-format=false --tlog-upload=false --key cosign.key --output-signature blob.sig blob
 docker run --rm --user "$user" -e HOME=/work -v "$tmp:/work" -w /work "$image" verify-blob --insecure-ignore-tlog --key cosign.pub --signature blob.sig blob
 
 printf '%s\n' 'tampered payload' >"$tmp/blob"
